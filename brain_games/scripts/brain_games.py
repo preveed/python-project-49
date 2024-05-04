c006ae6648721f9bd8bf4c0c8f1a8ecaccb1e50d
Andrey Basov <andronbas@gmail.com>
2024-05-04 23:21:58 +0300

    add name asking

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env Python3
+
+from brain_games import cli
+
 def main():
-    print('Welcome to the Brain Games!')
+	print('Welcome to the Brain Games!\n')
+	cli.welcome_user()
 
 if __name__ == '__main__':
-    main()
+	main()
