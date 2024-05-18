aab17eb0b3be64b90b5cd1b3170db90aa53a11f5
Andrey Basov <andronbas@gmail.com>
2024-05-18 22:52:53 +0300

    fix calc

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -4,20 +4,18 @@ import operator
 
 RULES = 'What is the result of the expression?\n'
 
-def get_math_operator(math_operator):
-    math_operator = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    }.get
-    return math_operator
+get_operator = {
+'+': operator.add,
+'-': operator.sub,
+'*': operator.mul,
+}.get
 
 def start_game():
     operatot_list = ['+', '-', '*']
     first_num = random.randint(1, 100)
     second_num = random.randint(1, 100)
     sign = random.choice(operatot_list)
-    operator = get_math_operator(sign)
+    operator = get_operator(sign)
     right_answer = str(operator(first_num, second_num))
     qwestion = f"{first_num} {sign} {second_num}"
     return qwestion, right_answer
