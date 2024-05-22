0db066fd848cab989dc694aeb5c14b1c88ac1ba2
Andrey Basov <andronbas@gmail.com>
2024-05-22 23:08:15 +0300

    update engine blind input

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,21 +1,18 @@
-import prompt
-
 NUMBER_OF_ROUNDS = 3
-
 
 def run(game):
     print('Welcome to the Brain Games!')
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
+    name = input('May I have your name? ')
+    print(f'Hello, {name}!')
     print(game.RULES)
     for _ in range(NUMBER_OF_ROUNDS):
-        question, right_answer = game.start_game()
-        print(f"Question: {question}")
-        user_answer = prompt.string('Your answer: ')
+        qestion, right_answer = game.start_game()
+        print(f'Qestion: {qestion}')
+        user_answer = input('Your answer: ')
         if user_answer == right_answer:
             print('Correct!')
         else:
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was"
-                  f" '{right_answer}'.\nLet's try again, {user_name}!")
+            f"'{right_answer}'.\nLet's try again, {name}!")
             return
-    print(f'Congratulations, {user_name}!')
+    print(f'Congratulations, {name}')
