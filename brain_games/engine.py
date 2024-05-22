c21530f9962c29cec1704def27d1ea2421946866
Andrey Basov <andronbas@gmail.com>
2024-05-22 23:13:51 +0300

    update engine blind input

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -2,13 +2,13 @@ NUMBER_OF_ROUNDS = 3
 
 def run(game):
     print('Welcome to the Brain Games!')
-    name = input('May I have your name? ')
+    name = str(input('May I have your name? '))
     print(f'Hello, {name}!')
     print(game.RULES)
     for _ in range(NUMBER_OF_ROUNDS):
         qestion, right_answer = game.start_game()
         print(f'Qestion: {qestion}')
-        user_answer = input('Your answer: ')
+        user_answer = str(input('Your answer: '))
         if user_answer == right_answer:
             print('Correct!')
         else:
